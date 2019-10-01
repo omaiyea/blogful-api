4167b8fe5a041149fb1e5279957fb5e8852d75c3
omaiyea <omaiyea@gmail.com>
2019-10-01 12:05:07 -0700

    add automated test cases then add functions to support

---
+++ postgrator-config.js
@@ -0,0 +1,9 @@
+//config file to read the db connection 
+//will be used by postgrator cli
+require('dotenv').config();
+
+module.exports = {
+    "migrationsDirectory": "migrations",
+    "driver": "pg",
+    "connectionString": (process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URL : process.env.DB_URL)
+}
--- src/app.js
+++ src/app.js
@@ -7,11 +7,36 @@ const helmet = require('helmet');
 
 const app = express();
 const { NODE_ENV } = require('./config');
+const ArticlesService = require('./articles-service')
+
 const morganOption = (NODE_ENV === 'production' ? 'tiny' : 'common');
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+   .then(articles => {
+     res.json(articles)
+   })
+   .catch(next) //handle errors with error handling middleware
+})
+
+app.get('/articles/:article_id', (req, res,next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+   .then(article => {
+     if(!article){
+       return res.status(404).json({
+         error: { message: `Article doesn't exist`}
+       })
+     }
+     res.json(article)
+   })
+   .catch(next)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
